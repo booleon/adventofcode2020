26030e8ad41fc16f355721870c3ec0e079f8abfe
Florimond Boquet <florimond.boquet@ext.cdiscount.com>
2020-12-01 19:39:07 +0100

    firs star

---
+++ adventofcode/__main__.py
@@ -0,0 +1,18 @@
+import logging
+from pathlib import Path
+
+from adventofcode.expense import get_number_summing_2020
+logging.basicConfig(level=logging.INFO)
+
+logging.info("start file")
+
+file_path = Path('.') / "advent_data"
+file_path.resolve()
+logging.info(f"filepath is : {file_path.as_posix()}")
+
+with file_path.open() as file:
+    lines = [int(line.rstrip()) for line in file]
+
+numbers = get_number_summing_2020(lines)
+
+print(numbers[0] * numbers[1])
