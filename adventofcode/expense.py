025ac637b640f05ce96564117f7f1db698e65e8e
Florimond Boquet <florimond.boquet@ext.cdiscount.com>
2020-12-01 21:08:22 +0100

    red: but improved

--- adventofcode/expense.py
+++ adventofcode/expense.py
@@ -1,13 +1,35 @@
-def is_equal_to_2020(entry1: int, entry2: int):
-    return entry1 + entry2 == 2020
+from typing import List
 
 
-def get_number_summing_2020(entries):
+def is_equal_to_2020(*entries):
+    return sum(entries) == 2020
+
+
+def get_number_summing_2020(entries: list):
     local_entries = entries[:]
-    potential_result = ()
+    potential_result = []
     while local_entries and not potential_result:
         first_number = local_entries.pop(0)
-        potential_result = [(first_number, other_entry)
-                            for other_entry in local_entries
-                            if is_equal_to_2020(first_number, other_entry)]
-    return potential_result[0]
+        potential_result = _get_missing_number_to_2020((first_number,), local_entries)
+    return potential_result
+
+
+def get_3_number_summing_2020(entries: list):
+    local_entries = entries[:]
+    potential_result = ()
+    while len(local_entries) > 2 and not potential_result:
+        first_number = local_entries.pop(0)
+        second_number = local_entries.pop(0)
+        potential_result = _get_missing_number_to_2020((first_number, second_number), entries)
+        local_entries.append(second_number)
+    return potential_result
+
+
+def _get_missing_number_to_2020(fist_items: tuple, entries: List[int]) -> tuple:
+    potential_result = [(fist_items + (other_number,))
+                        for other_number in entries
+                        if is_equal_to_2020(other_number, *fist_items)
+                        ]
+    if potential_result:
+        return potential_result[0]
+    return ()
--- tests/test_expense.py
+++ tests/test_expense.py
@@ -1,4 +1,4 @@
-from adventofcode.expense import is_equal_to_2020, get_number_summing_2020
+from adventofcode.expense import is_equal_to_2020, get_number_summing_2020, get_3_number_summing_2020
 
 
 class TestIsEqualTo2020:
@@ -20,9 +20,25 @@ class TestIsEqualTo2020:
         # assert
         assert result is False
 
+    def test_given_three_entry_which_summ_equal_2020_return_true(self):
+        # arrange
+        entries = (979, 366, 675)
+        # act
+        result = is_equal_to_2020(*entries)
+        # assert
+        assert result is True
+
+    def test_given_three_entry_which_sum_not_equal_2020_return_false(self):
+        # arrange
+        entries = (979, 55, 675)
+        # act
+        result = is_equal_to_2020(*entries)
+        # assert
+        assert result is False
+
 
 class TestGetNumberSumming2020:
-    def test_given_list_of_number_return_tuple_of_result(self):
+    def test_given_sorted_list_of_number_return_tuple_of_result(self):
         # arrange
         entries = [1721, 979, 366, 299, 675, 1456]
         # act
@@ -41,3 +57,27 @@ class TestGetNumberSumming2020:
         # assert
         assert 1721 in result
         assert 299 in result
+
+
+class TestGet3NumberSumming2020:
+    def test_given_sorted_list_return_tuple_of_result(self):
+        # arrange
+        entries = [979, 366, 1721, 299, 675, 1456]
+        # act
+        result = get_3_number_summing_2020(entries)
+
+        # assert
+        assert 979 in result
+        assert 366 in result
+        assert 675 in result
+
+    def test_given_unsorted_list_return_tuple_of_result(self):
+        # arrange
+        entries = [366, 1721, 979, 299, 675, 1456]
+        # act
+        result = get_3_number_summing_2020(entries)
+
+        # assert
+        assert 979 in result
+        assert 366 in result
+        assert 675 in result
